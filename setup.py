e0e2850d6a711cb61f67ee608c558c2faa09807d
Mark Harrison <mark@mivok.net>
2013-05-21 21:26:08 -0400

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup (
+    name="omnifab",
+    version="0.0.1",
+    description="Collection of helper functions for use with fabric",
+    author="Mark Harrison",
+    author_email="mark@omniti.com",
+    url="http://github.com/omniti-labs/omnifab",
+    license="MIT",
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Systems Administration"
+    ],
+    packages=['omnifab']
+)
